e4b000d5735db1500b056121dc38e19ef64fa384
Leon Valencia <techie@lynx.rocks>
2021-04-25 23:22:40 +0800

    add iam policy for metric subscription

--- lib/realtime-metric-stack.ts
+++ lib/realtime-metric-stack.ts
@@ -1,8 +1,8 @@
-import { Construct, Stack, StackProps, Duration, CustomResource } from '@aws-cdk/core';
+import { Construct, Stack, StackProps, Duration, CustomResource, Arn } from '@aws-cdk/core';
 import { Function, Runtime, Code } from '@aws-cdk/aws-lambda';
 import { Provider } from '@aws-cdk/custom-resources';
 import { RetentionDays } from '@aws-cdk/aws-logs';
-//import { PolicyStatement } from '@aws-cdk/aws-iam';
+import { PolicyStatement, Effect } from '@aws-cdk/aws-iam';
 
 export interface MetricProps {
   distributionId: string,
@@ -12,22 +12,29 @@ export class RealtimeMetricStack extends Stack {
 
   constructor(scope: Construct, id: string, metricProps: MetricProps, props?: StackProps) {
     super(scope, id, props);
-
+    const distributionArn = Arn.format({
+      service: 'cloudfront',
+      resource: 'distribution',
+      resourceName: metricProps.distributionId,
+    }, this);
+    const subscriptionPolicy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        'cloudfront:CreateMonitoringSubscription',
+      ],
+      resources: [
+        distributionArn,
+      ]
+    });
     const subscriptionHandler = new Function(this, 'SubscriptionHandler', {
       runtime: Runtime.PYTHON_3_8,
       handler: 'realtime_metric.on_event',
       code: Code.fromAsset(`${__dirname}/handler`),
       timeout: Duration.minutes(1),
       initialPolicy: [
-        // new PolicyStatement({
-        //   actions: ["s3:GetObject",
-        //     "s3:ListBucket"],
-        //   resources: [`arn:${Aws.PARTITION}:s3:::${sourceBucket}`,
-        //   `arn:aws:s3:::${sourceBucket}/${sourcePrefix}*`]
-        // }),
+        subscriptionPolicy,
       ]
     });
-
     const subscriptionProvider = new Provider(this, 'SubscriptionProvider', {
       onEventHandler: subscriptionHandler,
       logRetention: RetentionDays.ONE_DAY,
